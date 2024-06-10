9ee00960ee271006ede479350aa18a3cb0a00aa6
Steve Holden <steve@holdenweb.com>
2024-06-10 22:32:24 +0100

    Include libraries factored out from earlier versions.  On branch blueprints  Changes to be committed: 	new file:   pdf.py 	new file:   rotate.py 	new file:   uwsgi.ini

---
+++ src/hwpdf/views.py
@@ -0,0 +1,85 @@
+"""
+pdf.py: Various bits of PDF functionality
+"""
+from io import BytesIO
+from logging import getLogger
+from zipfile import ZipFile
+
+from booklet import make_booklet
+from flask_wtf import FlaskForm
+from flask import (Blueprint, Flask, Response, render_template, flash,
+                   send_file, redirect, request, url_for)
+import pdfrw
+from wtforms import FileField, SubmitField, StringField
+from wtforms.validators import DataRequired
+
+logger = getLogger(__name__)
+pdf_blueprint = Blueprint("PDF Handling", __name__)
+
+
+class BookletForm(FlaskForm):
+    file_details = FileField('file_details', validators=[DataRequired()])
+    submit = SubmitField("Generate Booklet")
+
+
+class PDF_Form(FlaskForm):
+    file_details = FileField('file_details', validators=[DataRequired()])
+    file_prefix = StringField('file_prefix')
+    submit = SubmitField('Get Pages')
+
+@pdf_blueprint.route("/", methods=['GET'])
+def root_page():
+    return Response("Here are some PDF things you can do!")
+
+@pdf_blueprint.route("/pagezip", methods=['GET', 'POST'])
+def get_or_post_pagezip():
+    form = PDF_Form()
+    if form.validate_on_submit():
+        my_file = request.files['file_details']
+        file_prefix = request.form['file_prefix'] or 'page'
+        try:
+            inputpdf = pdfrw.PdfReader(fname=my_file.stream)
+            outzip = BytesIO()
+            container = ZipFile(outzip, 'w')
+            for i, page in enumerate(inputpdf.pages):
+                output = pdfrw.PdfWriter()
+                file_name = f"{file_prefix}_{i+1:03}.pdf"
+                output.addpage(page)
+                outfile = BytesIO()
+                output.write(outfile)
+                container.writestr(f"pdf/{file_name}",
+                                   outfile.getvalue())
+            container.close()
+            outzip.seek(0)
+            return send_file(outzip,
+                             mimetype="application/octet-stream",
+                             as_attachment=True,
+                             download_name="pages.zip")
+        except Exception:
+            flash(f"Could not open file as a PDF - please try again")
+            raise
+    return render_template('pagesplit_form.html', form=form)
+
+
+@pdf_blueprint.route("/booklet", methods=['GET', 'POST'])
+def get_or_post_booklet():
+    form = BookletForm()
+    logger.info("Booklet requested")
+    if form.validate_on_submit():
+        my_file = request.files['file_details']
+        try:
+            output_pdfs = make_booklet(my_file.stream)
+            outzip = BytesIO()
+            container = ZipFile(outzip, 'w')
+            for p_typ, pdf in zip(('odd', 'even'), output_pdfs):
+                container.writestr(f"pdf/{p_typ}.pdf",
+                                   pdf.getvalue())
+            container.close()
+            outzip.seek(0)
+            return send_file(outzip,
+                             mimetype="application/zip",
+                             as_attachment=True,
+                             download_name="pages.zip")
+        except Exception as e:
+            return f"I'm sorry, Dave, it seems I couldn't do that:\n{e}"
+    return render_template('booklet_form.html', form=form)
